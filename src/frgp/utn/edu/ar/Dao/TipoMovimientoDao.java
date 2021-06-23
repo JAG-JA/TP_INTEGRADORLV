185341cea1efa8f96220b4112b08ad40cb5a4346
MNazarre <mnazarre@gmail.com>
2021-06-22 22:25:15 -0300

    dao tipomovimiento

---
+++ src/frgp/utn/edu/ar/Dao/TipoMovimientoDao.java
@@ -0,0 +1,8 @@
+package frgp.utn.edu.ar.Dao;
+
+import frgp.utn.edu.ar.generic.Crud;
+import frgp.utn.edu.ar.models.TipoMovimiento;
+
+public interface TipoMovimientoDao extends Crud<TipoMovimiento>{
+
+}
